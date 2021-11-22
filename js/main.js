1040a7ed5164bc6173b9a17462affb98ab921fbe
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-22 16:12:33 +0100

    add logical js structure

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// cliccando il menù hamburger tutto lo sfodo diventa grigio chiaro, in alto a destra ho una X che mi riporta alla condizione normale, e ongi li occupa una riga ed è bvianco e sottolineato.
+
+// 1. quando clicco l'hamburger viene sostituito da una croce
+
+// 2. lo sfondo della pagina diventa grigio chiaro:rgb(22, 28, 59)
+
+// 3.ogni a dentro li occuoa una riga ed è sottolineato
+
+// 4. cliccando sulla X tutto toprna normale
+
+
