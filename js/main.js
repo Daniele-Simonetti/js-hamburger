de52bb79ba8e7f23b1b265e59394a71687fbff4f
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-22 16:34:12 +0100

    add new logic structure

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 // cliccando il menù hamburger tutto lo sfodo diventa grigio chiaro, in alto a destra ho una X che mi riporta alla condizione normale, e ongi li occupa una riga ed è bvianco e sottolineato.
 
-// 1. quando clicco l'hamburger viene sostituito da una croce
+// far si che quando clicco sull'hamburger si attivi il div con classe hamburger-mune e active e quando riclicco che torni tutto normale
 
-// 2. lo sfondo della pagina diventa grigio chiaro:rgb(22, 28, 59)
+// 1. aggiungere la classe active all'hamburger menù quando clicco l'icona hamburger
 
-// 3.ogni a dentro li occuoa una riga ed è sottolineato
+//  2. quando clicco sull'icona della croce rimuovo la classe active dall'hamburger menu
 
-// 4. cliccando sulla X tutto toprna normale
 
 
