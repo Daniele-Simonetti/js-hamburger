428d96dba0c35c4c8595a7fa7e5d126ae1acdb5c
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-22 17:03:12 +0100

    testing

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@
 // far si che quando clicco sull'hamburger si attivi il div con classe hamburger-mune e active e quando riclicco che torni tutto normale
 
 // 1. aggiungere la classe active all'hamburger menù quando clicco l'icona hamburger
+const HamburgerHideList = document.querySelector('.hamburger-menu');
+console.log(HamburgerHideList.classList.value);
 
+const HamburgerBtn = document.querySelector('div.header-right a')
+
+HamburgerBtn.addEventListener('click', function (){
+  HamburgerHideList.classList.add ('active');
+});
+console.log(HamburgerHideList.classList.value);
 //  2. quando clicco sull'icona della croce rimuovo la classe active dall'hamburger menu
 
 
